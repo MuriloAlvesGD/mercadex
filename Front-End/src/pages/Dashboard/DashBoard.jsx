b6aec69755fecff8a716a900b110634b9b1b4404
MuriloAlvesGD <luriomgdevelop@gmail.com>
2024-09-03 16:10:15 -0300

    Search button fixed and add chart.js

--- Front-End/src/pages/Dashboard/DashBoard.jsx
+++ Front-End/src/pages/Dashboard/DashBoard.jsx
@@ -2,6 +2,41 @@ import "./DashBoard.css"
 import {Carousel} from "../../components/Carousel/Carousel.jsx";
 import CarouselItem from "../../components/Carousel/CarouselItem.jsx";
 import DefaultImageIcon from "../../assets/defaultImageIcon.png";
+import { Chart as ChartJS,CategoryScale,LinearScale,BarElement,Title,Tooltip,Legend,} from 'chart.js';
+ChartJS.register(CategoryScale,LinearScale,BarElement,Title,Tooltip,Legend);
+import {Bar} from 'react-chartjs-2';
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            position: 'top',
+            text: 'Data',
+            color: '#df5201',
+        },
+        layout: {
+            padding: 100,
+        }
+    },
+};
+
+const labels = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho"];
+const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset vermelho',
+            data: labels.map((() => Math.floor(Math.random() * 100))),
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderColor: 'red',
+            borderWidth: 1
+        },
+    ],
+};
 
 function DashBoard() {
     return (
@@ -14,9 +49,11 @@ function DashBoard() {
                             <div className="dashboard-item">
                                 <main className="left-section">
                                     <h1 className="dashboard-title">Produto Mais Vendido</h1>
+                                    <Bar options={options} data={data}/>
                                 </main>
                                 <div className="right-section">
-                                    <img src={DefaultImageIcon} alt="best seller product" className="best-seller-product"/>
+                                    <img src={DefaultImageIcon} alt="best seller product"
+                                         className="best-seller-product"/>
                                     <div className="dashboard-item-text">
                                         <h1>Nome do Produto</h1>
                                         <h2>preço</h2>
